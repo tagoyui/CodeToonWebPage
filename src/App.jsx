4f546f549a3f9a95a1a1f132d85b6f6d8a3399fd
tagoyui <yui@tagotomotadanoMacBook-Pro.local>
2023-01-25 23:02:54 +0900

    blackbace

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="hero is-dark is-bold">
+    <header className="hero is-black is-bold">
       <div className="hero-body">
         <div className="container">
           <h1 className="title">COOD TOON</h1>
@@ -10,10 +10,20 @@ function Header() {
   );
 }
 
+function Body() {
+  return (
+    <div className="hero success id-dark is-fullheight">
+      <div className="hero-body">
+        <Main />
+      </div>
+    </div>
+  );
+}
+
 function Main() {
   return (
     <div>
-      <div className="card">
+      <div className="card has-background-black  has-text-white">
         <div className="card-content">
           <div className="media">
             <div className="media-left">
@@ -21,9 +31,7 @@ function Main() {
                 <img src="image/icon.png"></img>
               </figure>
             </div>
-            <div className="media-content">
-              <p className="title">概要</p>
-            </div>
+            <p className="title has-text-white">概要</p>
           </div>
           <div className="content">
             ゲーム形式で楽しくプログラミングが学習できる対戦型パズルゲーム
@@ -31,7 +39,7 @@ function Main() {
         </div>
       </div>
 
-      <div className="card">
+      <div className="card has-background-black  has-text-white">
         <div className="card-content">
           <div className="media">
             <div className="media-left">
@@ -39,9 +47,7 @@ function Main() {
                 <img src="image/icon.png"></img>
               </figure>
             </div>
-            <div className="media-content">
-              <p className="title">ルール</p>
-            </div>
+            <p className="title has-text-white">ルール</p>
           </div>
           <div className="content">
             適当なプログラムを入力することによって相手の陣地を攻撃や自分の陣地を防御できる相手の陣地を全て制覇することで勝利となる。
@@ -49,7 +55,7 @@ function Main() {
         </div>
       </div>
 
-      <div className="card">
+      <div className="card has-background-black  has-text-white">
         <div className="card-content">
           <div className="media">
             <div className="media-left">
@@ -57,9 +63,7 @@ function Main() {
                 <img src="image/icon.png"></img>
               </figure>
             </div>
-            <div className="media-content">
-              <p className="title">操作</p>
-            </div>
+            <p className="title has-text-white">操作</p>
           </div>
           <div className="content">
             同じWiFiに繋がった2台のパソコン同士で対戦ができる。
@@ -68,7 +72,7 @@ function Main() {
         </div>
       </div>
 
-      <div className="card">
+      <div className="card has-background-black  has-text-white">
         <div className="card-content">
           <div className="media">
             <div className="media-left">
@@ -76,46 +80,48 @@ function Main() {
                 <img src="image/icon.png"></img>
               </figure>
             </div>
-            <div className="media-content">
-              <p className="title">コードと使用例</p>
-            </div>
+            <p className="title has-text-white">コードと使用例</p>
           </div>
           <div className="content">
-            <table class="table">
-              <tr>
-                <th>名前</th>
-                <th>説明</th>
-                <th>使用例</th>
-              </tr>
-              <tr>
-                <td>attack</td>
-                <td>メモリを指定して攻撃</td>
-                <td>attack(enemy.memory[0][0]);</td>
-              </tr>
-              <tr>
-                <td>setPass</td>
-                <td>メモリにロックをかける</td>
-                <td>memory[0][0].setPass(0000);</td>
-              </tr>
-              <tr>
-                <td>connect</td>
-                <td>メモリにアクセスしてコードを表示</td>
-                <td>memory[0][0].connect()</td>
-              </tr>
-              <tr>
-                <td>if文</td>
-                <td></td>
-                <td>
-                  if(Boolean i){"{"}INSIDE{"}"}
-                </td>
-              </tr>
-              <tr>
-                <td>for文</td>
-                <td></td>
-                <td>
-                  for(int value; Boolean i; action){"{"}INSIDE{"}"}
-                </td>
-              </tr>
+            <table className="table has-background-black  has-text-white">
+              <thead className="">
+                <tr>
+                  <th>名前</th>
+                  <th>説明</th>
+                  <th>使用例</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>attack</td>
+                  <td>メモリを指定して攻撃</td>
+                  <td>attack(enemy.memory[0][0]);</td>
+                </tr>
+                <tr>
+                  <td>setPass</td>
+                  <td>メモリにロックをかける</td>
+                  <td>memory[0][0].setPass(0000);</td>
+                </tr>
+                <tr>
+                  <td>connect</td>
+                  <td>メモリにアクセスしてコードを表示</td>
+                  <td>memory[0][0].connect()</td>
+                </tr>
+                <tr>
+                  <td>if文</td>
+                  <td></td>
+                  <td>
+                    if(Boolean i){"{"}INSIDE{"}"}
+                  </td>
+                </tr>
+                <tr>
+                  <td>for文</td>
+                  <td></td>
+                  <td>
+                    for(int value; Boolean i; action){"{"}INSIDE{"}"}
+                  </td>
+                </tr>
+              </tbody>
             </table>
           </div>
         </div>
@@ -128,7 +134,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Main />
+      <Body />
     </div>
   );
 }
